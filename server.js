cbd8725d4d7f35a48129750245655a4fa5ef3980
RecursiveIteration <James.C.Woodson@gmail.com>
2017-07-31 15:46:30 -0700

    server is operational

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const express = require('express');
+const pg = require('pg');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// const conString = 'postgres://localhost:5432/lunchbuddies'
+const conString = process.env.conString || `postgres://postgres:${process.env.PG_PASSWORD}@localhost:5432/lunchbuddies`;
+const client = new pg.Client(conString);
+client.connect();
+client.on('error', err => console.error(err));
+
+app.use(express.static('./public'));
+
+app.listen(PORT, () => console.log(`app listening on port: ${PORT}`));
