30e9960e9b0d6e8ca7bb2fa58a3672436433f069
Don <don@Dons-Mac-Pro.local>
2017-08-01 20:52:54 -0700

    fixed match insertion

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const pg = require('pg');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-// const conString = 'postgres://localhost:5432/lunchbuddies'
-const conString = process.env.conString || `postgres://postgres:${process.env.PG_PASSWORD}@localhost:5432/lunchbuddies`;
+const conString = 'postgres://localhost:5432/lunchbuddies'
+// const conString = process.env.conString || `postgres://postgres:${process.env.PG_PASSWORD}@localhost:5432/lunchbuddies`;
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
@@ -65,16 +65,60 @@ app.post('/roster', function(req, res) {
 })
 
 
-app.post('/matches', function(req, res) {
+// app.post('/matches', function(req, res) {
+//
+  // client.query(`
+  //   SELECT max(round) FROM rounds;
+  //   `)
+  //   .then(previousRound => {
+  //     previousRound.rows[0].max;
+  //   })
+// })
 
+app.post('/matches', function(req, res) {
+  console.log('test')
+  console.log(req.body.matches);
   client.query(`
-    SELECT max(round) FROM rounds;
-    `)
-    .then(previousRound => {
-      previousRound.rows[0].max;
+    SELECT max(round) as round, max(match_id) as match FROM rounds;
+    `).then(results => {
+      let round = results.rows[0].round || 0;
+      let match = results.rows[0].match || -1;
+      round++;
+      match++;
+      insertMatches(round, match, req.body.matches);
     })
 })
 
+function insertMatches(maxRound, maxMatch, matches) {
+  for (var i=0; i < matches.length; i++) {
+    client.query(`
+      INSERT INTO rounds(round)
+      VALUES ('${maxRound}');
+      `,
+    function (e) {
+      if (e) {
+        console.error(e);
+      } else {
+        // res.send('insert complete');
+      }
+    })
+    for (var p=0; p < matches[i].length; p++) {
+      let currentMatch = maxMatch + i
+      let person = matches[i][p]
+      client.query(`
+        INSERT INTO matches(match_id, id)
+        VALUES ('${currentMatch}', '${person}');
+        `,
+      function (e) {
+        if (e) {
+          console.error(e);
+        } else {
+          // res.send('insert complete');
+        }
+      })
+    }
+  }
+}
 
 
 // app.post('/matches', function(req, res) {
